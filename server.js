c90e543f633e6bb42461adb2fffe86b902a70276
Niyitangasam <niyitangasam@gmail.com>
2019-01-04 21:55:09 +0200

    Add Driver routes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+require('./routes/driversRoutes')(app);
+
+console.log('in server.js');
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on Port', PORT);
