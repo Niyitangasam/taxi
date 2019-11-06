4a526aa49385d1591f9f60621bc5e7c4aef946b1
Niyitangasam <niyitangasam@gmail.com>
2019-11-06 20:09:00 +0200

    Add trip functionality

--- controller/trips.js
+++ controller/trips.js
@@ -9,41 +9,46 @@ const { Trip } = models;
  * @description Handles all related trip functianalities
  * */
 class TripController {
-  static create(req, res) {
-    const {
-      startLocation, endLocation, driverId, riderId, completed,
-    } = req.body;
-    return Trip.create({
-      startLocation, endLocation, driverId, riderId, completed,
-    })
-      .then(trip => res.send(trip))
-      .catch((err) => {
-        res.send(200, JSON.stringfy(err));
-        return res(err);
+  static async create(req, res) {
+    try {
+      const {
+        startLocation, endLocation, driverId, riderId,
+      } = req.body;
+      await Trip.create({
+        startLocation,
+        endLocation,
+        driverId,
+        riderId,
       });
+      return res.status(200).send({ message: 'Trip created successfully' });
+    } catch (error) {
+      return res.status(500).send(error);
+    }
   }
 
   // complete a trip
 
-  static complete(req, res, next) {
-    return Trip.update(
-      { completed: true },
-      { returning: true, where: { id: req.params.id } },
-    )
-      .then((updatedBook) => {
-        res.send(updatedBook);
-      })
-      .catch(next);
+  static async complete(req, res) {
+    try {
+      const completedTrip = await Trip.update(
+        { completed: true },
+        { returning: true, where: { id: req.params.id } },
+      );
+      return res.status(200).send(completedTrip);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
   }
 
   // Get a list of active trip
 
-  static getAllActive(req, res) {
-    return Trip.findAll({ where: { completed: true } })
-      .then(trip => res.send(trip))
-      .catch((err) => {
-        res.status(400).send(err);
-      });
+  static async getAllActive(req, res) {
+    try {
+      const activeTrip = await Trip.findAll({ where: { completed: true } });
+      return res.status(200).send(activeTrip);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
   }
 }
 export default TripController;
