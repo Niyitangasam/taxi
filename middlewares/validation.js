20fb6a622a18d1b167f5d8b281c7251770b3b620
Niyitangasam <niyitangasam@gmail.com>
2019-11-06 21:36:23 +0200

    Add middleware for validating params

---
+++ middlewares/validation.js
@@ -0,0 +1,25 @@
+
+/**
+ * @exports ValidationMiddleware
+ * @class ValidationMiddleware
+ * @description a class to validate paramaters
+ * */
+class ValidationMiddleware {
+  static validateId(req, res, next) {
+    const { id } = req.params;
+    if ((id % 1) !== 0) {
+      return res.status(400).json({ status: 400, message: 'ID must be number!' });
+    }
+    return next();
+  }
+
+  static validateString(req, res, next) {
+    const { location } = req.params;
+    const regexString = /(.*[a-zA-Z].*)/;
+    if (!regexString.test(location)) {
+      return res.status(400).json({ status: 400, message: 'Location must be a string' });
+    }
+    return next();
+  }
+}
+export default ValidationMiddleware;
--- routes/trips.js
+++ routes/trips.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import TripController from '../controller/trips';
+import ValidationMiddleware from '../middlewares/validation';
+
 
 const router = express.Router();
 
 
 router.post('/', TripController.create);
-router.put('/complete/:id', TripController.complete);
+router.put('/complete/:id', ValidationMiddleware.validateId, TripController.complete);
 router.get('/active', TripController.getAllActive);
 
 export default router;
